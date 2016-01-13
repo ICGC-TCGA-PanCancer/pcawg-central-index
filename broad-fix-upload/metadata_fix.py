0b01e982bfbf598c554f0425898a41553eee64c0
lindaxiang <linda.xiang@oicr.on.ca>
2016-01-13 14:13:58 -0500

    first test run well

--- broad-fix-upload/metadata_fix.py
+++ broad-fix-upload/metadata_fix.py
@@ -49,8 +49,7 @@ def get_formal_repo_name(repo):
     return repo_url_to_repo.get(repo)
 
 
-def download_metadata_xml(gnos_id, gnos_repo, download_dir):
-    metadata_xml_dir = os.path.join(download_dir, gnos_id)
+def download_metadata_xml(gnos_id, gnos_repo, download_dir=None):
     logger.info('Download metadata xml from GNOS repo: {} for analysis object: {}'.format(gnos_repo, gnos_id))
     
     url = gnos_repo + 'cghub/metadata/analysisFull/' + gnos_id
@@ -66,10 +65,12 @@ def download_metadata_xml(gnos_id, gnos_repo, download_dir):
         sys.exit('Unable to download GNOS metadata xml, please check the log for details.')
     else:
         metadata_xml_str = response.text
-
-        metadata_xml_file = metadata_xml_dir + '/' + gnos_id  + '.xml'
-        with open(metadata_xml_file, 'w') as f:  # write to metadata xml file now
-            f.write(metadata_xml_str.encode('utf8'))
+        if download_dir:
+            metadata_xml_file = os.path.join(download_dir, gnos_id, gnos_id+'.xml')
+            with open(metadata_xml_file, 'w') as f:  # write to metadata xml file now
+                f.write(metadata_xml_str.encode('utf8'))
+        else:
+            return metadata_xml_str
 
 def download_datafiles(gnos_id, gnos_repo, download_dir):
     url = gnos_repo + 'cghub/data/analysis/download/' + gnos_id
@@ -96,46 +97,6 @@ def download_datafiles(gnos_id, gnos_repo, download_dir):
 def generate_uuid():
     uuid_str = str(uuid.uuid4())
     return uuid_str
-
-
-# def update_fixed_data_block_files(data_block_files, workflow_type, upload_dir, download_dir, gnos_id):
-#     lookup_dir = os.path.join(download_dir, gnos_id)
-#     fname_all = []
-#     data_block_files_new = []
-#     for f in data_block_files:
-#         fname = str(f.get('@filename'))
-#         if not fname in fname_all:
-#             fname_all.append(fname)
-#             fname_list = str.split(fname, '.')
-#             fname_list[2] = '[0-9]*[0-9]'
-#             fname_search = '.'.join(fname_list) 
-#             f_fixed = glob.glob(lookup_dir + '/fixed_files/' + fname_search)
-#             if not f_fixed:
-#                 f_old = glob.glob(lookup_dir + '/' + fname_search )
-#                 if len(f_old) == 1:
-#                     f_checksum = generate_md5(f_old[0]) 
-#                     if not f_checksum == f.get('@checksum'):
-#                         logger.warning('file: {} in the downloads folder has different checksum with the original one for analysis object: {}'.format(fname, gnos_id))
-#                         return
-#                 elif not f_old:
-#                     logger.warning('file: {} is missing in the downloads folder for analysis object: {}'.format(fname, gnos_id))
-#                     return
-#                 else:
-#                     logger.warning('file: {} has duplicates in the downloads folder for analysis object: {}'.format(fname, gnos_id))
-#                     return
-#             elif len(f_fixed) == 1:
-#                 f['@filename'] = f_fixed[0].split('/')[-1]
-#                 f['@checksum'] = generate_md5(f_fixed[0])
-                
-#             else:
-#                 logger.warning('file: {} has duplicates fixed files in the downloads folder for analysis object: {}'.format(fname, gnos_id))
-#                 return
-#             data_block_files_new.append(copy.deepcopy(f))
-
-#         else: # duplicates, not keep in the data_block_files_new
-#             logger.warning('file: {} is duplicated and removed from the data_block_files for analysis object: {}'.format(fname, gnos_id))            
-        
-#     return data_block_files_new
 
 
 def generate_md5(fname):
@@ -200,7 +161,6 @@ def metadata_fix(work_dir, donors_to_be_fixed):
             upload_dir = os.path.join(work_dir, 'uploads', get_formal_repo_name(donor.get(caller + '_gnos_repo')), upload_gnos_uuid)
         os.makedirs(upload_dir)
 
-
         gnos_entry_dir = os.path.join(work_dir, 'downloads', donor.get(caller + '_gnos_id'))
         fixed_file_dir = os.path.join(work_dir, '../fixed_files')
         xml_file = os.path.join(gnos_entry_dir, donor.get(caller + '_gnos_id') + '.xml')
@@ -212,11 +172,44 @@ def metadata_fix(work_dir, donors_to_be_fixed):
 
         create_fixed_gnos_submission(upload_dir, gnos_analysis_object) # merge xml
 
-        generate_updated_metadata(donor, upload_gnos_uuid)
+        generate_updated_metadata(donor, upload_gnos_uuid, work_dir)
 
 
-def generate_updated_metadata(donor, upload_gnos_uuid):
-    pass
+def generate_updated_metadata(donor, upload_gnos_uuid, work_dir):
+    for caller in ['muse', 'broad_tar']:
+        gnos_id = donor.get(caller+'_gnos_id')
+        gnos_repo = donor.get(caller+'_gnos_repo')
+        xml_str = download_metadata_xml(gnos_id, gnos_repo)
+        donor_unique_id = donor.get('donor_unique_id')
+        update_field = 'related_file_subset_uuids'
+        analysis_xml_str = modify_metadata(xml_str, update_field, caller, gnos_repo, gnos_id, donor.get('donor_unique_id'), upload_gnos_uuid, donor.get('broad_gnos_id'))
+        updated_metafiles_dir = os.path.join(work_dir, 'updated_metafiles', get_formal_repo_name(gnos_repo), gnos_id)
+        if not os.path.exists(updated_metafiles_dir):
+            os.makedirs(updated_metafiles_dir)
+        with open(updated_metafiles_dir+'/analysis.xml', 'w') as y:
+            y.write(analysis_xml_str)
+
+
+def modify_metadata(xml_str, update_field, caller, gnos_repo, gnos_id, donor_unique_id, new_gnos_id, old_gnos_id):
+    # we need to take care of xml properties in different order but effectively/semantically the same
+    gnos_analysis = xmltodict.parse(xml_str).get('ResultSet').get('Result')
+    if not gnos_analysis.get('analysis_id') == gnos_id or not gnos_analysis.get('dcc_project_code') == donor_unique_id.split('::')[0] \
+        or not gnos_analysis.get('participant_id') == donor_unique_id.split('::')[1]:
+        logger.warning('donor: {} has wrong gnos entry {} at repo: {}'.format(donor_uniqu_id, gnos_id, gnos_repo))
+        return None
+    analysis_xml = xmltodict.parse(xml_str).get('ResultSet').get('Result').get('analysis_xml')
+    for a in analysis_xml['ANALYSIS_SET']['ANALYSIS']['ANALYSIS_ATTRIBUTES']['ANALYSIS_ATTRIBUTE']:
+        if a['TAG'] == update_field:
+            uuid_set = set(a['VALUE'].split(','))
+            uuid_set.remove(old_gnos_id)
+            uuid_set.add(new_gnos_id)
+            a['VALUE'] = ','.join(list(uuid_set))
+            logger.info('donor: {} update the {} for {}_variant_calling with gnos_id: {} at gnos_repo: {}'.format(donor_unique_id, update_field, caller, gnos_id, gnos_repo))
+
+    analysis_xml = {'analysis_xml': analysis_xml}
+    analysis_xml_str = xmltodict.unparse(analysis_xml, pretty=True)
+
+    return analysis_xml_str
     
 
 
@@ -330,9 +323,9 @@ def get_files(gnos_analysis_object, fixed_file_dir, gnos_entry_dir):
 
             if matched_fp: file_name_patterns.remove(matched_fp)  # remove the file pattern that had a match
  
-    print matched_files
-
+    # print matched_files
     # print len(matched_files) 
+    
     for fp in file_name_patterns:
         logger.error('Missing expected variant call result file with pattern: {}'.format(fp))
         sys.exit('Missing expected variant call result file, see log file for details.')
@@ -358,8 +351,8 @@ def detect_folder(work_dir, folder_name):
 def main():
     if len(sys.argv) == 1: sys.exit('\nMust specify working directory where the variant call fixes are kept.\nPlease refer to the SOP for details how to structure the working directory.\n')
     work_dir = sys.argv[1]
-    # if os.path.isdir(work_dir): shutil.rmtree(work_dir, ignore_errors=True)  # empty the folder if exists
-    # os.makedirs(work_dir+'/downloads/broad')
+    if os.path.isdir(work_dir): shutil.rmtree(work_dir, ignore_errors=True)  # empty the folder if exists
+    os.makedirs(work_dir+'/downloads')
     work_dir = os.path.abspath(work_dir)
 
     if work_dir == os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test'):
@@ -391,7 +384,7 @@ def main():
     # now download metadata xml
     # if not test:
     print('\nDownloading data files and metadata XML from GNOS ...')
-    # download_metadata_files(work_dir, donors_to_be_fixed)
+    download_metadata_files(work_dir, donors_to_be_fixed)
 
     print('\nGenerating index files for fixed_files ...')
     # generate_index_files(work_dir, donors_to_be_fixed)
